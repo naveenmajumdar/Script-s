c127b5ec467c747c1cf220ab739697328b5e4f6a
naveenmajumdar <naveenmajumdar@gmail.com>
2018-07-12 19:48:13 +0530

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "creating snapshot"
-aws ec2 create-snapshot --region us-west-1 --output=text --description $HOSTNAME $DATE  --volume-id vol-08fc06fb3384758df
+aws ec2 create-snapshot --region us-west-1 --output=text --description $HOSTNAME $DATE  --volume-id <VALUM-ID>
